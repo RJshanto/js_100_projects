a2b10b29122b8401221790c544c6999b0ef2a039
Rifat Jahan <mdshantorjbd@gmail.com>
2023-03-19 17:40:25 +0600

    Initial commit

---
+++ first.js
@@ -0,0 +1,15 @@
+
+
+//first conceptual class with faisal vai ....
+
+
+
+console.log(5555);
+console.log(5555);
+console.log(5555);
+console.log(5555);
+
+var x=20;
+var y=40;
+var z=x+y;
+console.log(z);
