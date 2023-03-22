8eb1ee267ef8f1c3d54ca983947b37d6a4d2a08f
Rifat Jahan <mdshantorjbd@gmail.com>
2023-03-22 19:19:24 +0600

    initial commit

--- first.js
+++ first.js
@@ -11,3 +11,29 @@ var x=20;
 var y=40;
 var z=x+y;
 console.log(z);
+
+var str= "20";
+var num=20;
+
+var add =str+num;
+console.log(add);
+
+var num1 = 10;
+var num2 = "5";
+var num3 = num1*num2;
+
+console.log(num3);
+
+
+// multiplecation table create ...
+
+function multiplecationtable(num){
+
+    for(var i=1; i<=10; i++){
+        var cal= num +"*"+ i+"="+ num*i;
+       console.log(cal);
+    }
+  
+}
+var table=multiplecationtable(2);
+
